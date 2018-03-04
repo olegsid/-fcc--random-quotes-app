30232c93e87da22ee36b08c2e4072085562b8577
olegsid <olegsid93@gmail.com>
2018-03-04 18:53:34 +0200

    fix(): fix cross suport for git

--- app/js/dev/main.js
+++ app/js/dev/main.js
@@ -11,25 +11,26 @@ let quoteViewer = new QuoteViewer('author-name', 'wiki-text', 'wiki-button');
 const url = function() {
 	let key = Math.floor(Math.random() * 1000000 - 1);
 	return (
-		'http://cors-proxy.htmldriven.com/?url=https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&callback=?' +
-		key
+		'https://cors.now.sh/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&callback=?' + key
 	);
 };
 
-
-const displayMessage = ( data ) =>
+const displayMessageSubscriber = (data) =>
 	fetch(url())
-		.then( status )
-		.then( response => response.json())
-		.then( data => xmlToJson( parseXml(data.body) ) )
-		.then( obj => quoteViewer.displayNewQute(obj.forismatic.quote.quoteAuthor, obj.forismatic.quote.quoteText))
+		.then(status)
+		.then((response) => response.json())
+		.then((data) => quoteViewer.displayNewQute(data.quoteAuthor, data.quoteText));
 
-const colorSubscriber = ( function( selector ) {
-	const app = document.querySelector(selector);
-	return () => (app.style = `background-color:${randomHexColor()}; transition: background-color 1s linear;`);
-})('body');
+const changeColorSubscriber = (function(selector) {
+	const selected = document.querySelectorAll(selector);
+	let style;
+	return () => {
+		style = `background-color:${randomHexColor()}; transition: background-color 1s linear;`;
+		selected.forEach((item) => (item.style = style));
+	};
+})('.js-colored');
 
-const observsers = [ colorSubscriber, displayMessage ];
+const observsers = [ changeColorSubscriber, displayMessageSubscriber ];
 observsers.forEach((obs) => clickPublisher.subscribe(obs));
 quoteViewer.onClick(() => clickPublisher.deliver());
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Random qoute aplication
+
+live demo : [link](https://olegsid.github.io/-ffcc--random-quotes-app/)
+
+npm i && gulp watch
